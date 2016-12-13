ad1153a03520e5e73fc651f85ce99800e174fb10
Dzyuba Vlad <Dzyubavlad@gmail.com>
2016-12-14 00:49:44 +0200

    Fixed app function, added chain

--- JavaScript/6-functor-ap-fp.js
+++ JavaScript/6-functor-ap-fp.js
@@ -1,22 +1,14 @@
 'use strict';
 
+const mapNull = (fn, x) => x ? fn(x) : null;
+
 function maybe(x) {
-  let map = fn => (x && fn) ? fn(x) : null;
-
-  map.ap = map2 => (
-    maybe(
-      map(
-        mbValue => (
-          maybe(map2)(
-            mbFunction => mbFunction(mbValue)
-          )
-        )
-      )
-    )
-  );
-
+  const map = fn => maybe(mapNull(fn, x));
+  map.ap = fnA => fnA(fn => mapNull(fn, x));
+  map.chain = fnM => fnM(x);
   return map;
 }
 
-maybe(5).ap(x => ++x)(console.log);
-maybe(5).ap(x => x * 2).ap(x => ++x)(console.log);
+maybe(5)(x => x * 2)(x => ++x)(console.log);
+maybe(5)(x => x * 2).ap(maybe(x => ++x))(console.log);
+maybe(5).chain(x => maybe(x * 2))(x => ++x)(console.log);
